a4a435fe01a56a742c27a47bd7285a386b34527e
tigger-b <tigger-b@users.noreply.github.com>
2021-07-07 16:41:31 +1000

    computer always wins

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("welcome to rock paper scissors")
+print("1 rock, 2 paper, 3 scissors")
+for i in range(3):
+    val = input("Enter your value: ")
+    weapons = {'1': "rock", '2': "paper", "3":"scissors"}
+    print("you chose " + weapons[val])
 
+    cheat_wins = {
+        "rock": "paper",
+        "scissors": "rock",
+        "paper": "scissors"
+    }
+
+    print("Computer chooses " + cheat_wins[weapons[val]])
+    print("\u001b[31mYOU LOOSE AGAIN\u001b[37m")
